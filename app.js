12fc396cc78e5cdd3edc07cda36f7639dae38df4
tanzie1313 <tanziehickory@gmail.com>
2024-11-08 12:29:54 -0800

    commit

--- app.js
+++ app.js
@@ -41,7 +41,57 @@
 //   clearly indicate a win adn a loss
 
 ///*-------------------------------- Constants --------------------------------*/
+const wordList = [
+    {
+        word: 'astronaut',
+        hint: 'a person who is trained to travel into outer space'
+    },
+    {
+        word: 'alien',
+        hint: 'a creature from outer space'
+    },
+    {
+        word: 'spaceship',
+        hint: 'a vehicle used for travel in outer space'
+    },
+    {
+        word: 'planet',
+        hint: 'a celestial body moving in an elliptical orbit around a star'
+    },
+    {
+        word: 'comet',
+        hint: 'a celestial object consisting of a nucleus of ice and dust'
+    },
+    {
+        word: 'asteroid',
+        hint: 'a small rocky body orbiting the sun'
+    },
+    {
+        word: 'nebula',
+        hint: 'a cloud of gas and dust in outer space'
+    },
+    {
+        word: 'galaxy',
+        hint: 'a system of millions or billions of stars'
+    },
+    {
+        word: 'universe',
+        hint: 'all of space and everything in it'
+    },
+    {
+        word: 'blackhole',
+        hint: 'a region of space having a gravitational field so intense that no matter or radiation can escape'
 
+    }
+];
+const maxWrongGuesses = 6;
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const sounds {
+    win: new Audio('sounds/win.mp3'),
+    lose: new Audio('sounds/lose.mp3'),
+    correct: new Audio('sounds/correct.mp3'),
+    incorrect: new Audio('sounds/incorrect.mp3')
+};
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
