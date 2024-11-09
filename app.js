84741f4b5ca99e9f089b56b7cfa37711c6ef9be4
tanzie1313 <tanziehickory@gmail.com>
2024-11-08 19:01:00 -0800

    commit

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 // 
 // 1) Define any variables used to track the state of the game:
 //    guessedWord - the word to guess
-//    spaceship - the spaceship to build
+//    hint - a hint for the word to guess
 //    wrongLetters - the letters guessed that are not in the word
 //    currentWord - the word as it is currently being guessed
+//    remainingGuesses - the number of guesses remaining
 
 // 2) Define the required constants:
 //    1) The word to guess
-//    2) The spaceship to build
+//   2) The hint for the word to guess
 //    3) The number of wrong guesses allowed
 //    4) The number of wrong guesses made
 //    5) The number of correct guesses made
@@ -22,7 +23,7 @@
 //    13) The alphabet of letters to guess
 //  3)  We'll need a reference to a DOM element to display messages
 //    We'll need a reference to a DOM element to display the word being guessed
-//    We'll need a reference to a DOM element to display the spaceship being built
+//  
 //    We'll need a reference to a DOM element to display the letters guessed that are not in the word
 //    We'll need a reference to a DOM element to display the number of guesses remaining
 // 4) Define the required functions:
@@ -39,68 +40,68 @@
 // 6) Render a win message to the player 
 //    when player loses or doesn't win select the right letter the spaceship will be built
 //   clearly indicate a win adn a loss
+/*-------------------------------- Constants --------------------------------*/
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const maxWrongGuesses = 6;
 
-///*-------------------------------- Constants --------------------------------*/
-const wordList = [
-    {
-        word: 'astronaut',
-        hint: 'a person who is trained to travel into outer space'
-    },
-    {
-        word: 'alien',
-        hint: 'a creature from outer space'
-    },
-    {
-        word: 'spaceship',
-        hint: 'a vehicle used for travel in outer space'
-    },
-    {
-        word: 'planet',
-        hint: 'a celestial body moving in an elliptical orbit around a star'
-    },
-    {
-        word: 'comet',
-        hint: 'a celestial object consisting of a nucleus of ice and dust'
-    },
-    {
-        word: 'asteroid',
-        hint: 'a small rocky body orbiting the sun'
-    },
-    {
-        word: 'nebula',
-        hint: 'a cloud of gas and dust in outer space'
-    },
-    {
-        word: 'galaxy',
-        hint: 'a system of millions or billions of stars'
-    },
-    {
-        word: 'universe',
-        hint: 'all of space and everything in it'
-    },
-    {
-        word: 'blackhole',
-        hint: 'a region of space having a gravitational field so intense that no matter or radiation can escape'
+
+/*-------------------------------- Variables --------------------------------*/
+let currentWord; // the word to guess
+let wrongLetters; // the letters guessed that are not in the word
+let guessedLetters; // the letters guessed that are in the word
+let remainingGuesses; // the number of guesses remaining
+let wordHint; // the hint for the word to guess
+/*------------------------ Cached Element References ------------------------*/
+const wordDispleyEl = document.querySelector('.word-display');
+const chancesDisplayEl = document.querySelector('.nums-chances');
+const playAgainBtn = document.querySelector('.play-again');
+const HintEl = document.querySelector('.hint');
+const gameResultEl = document.querySelector('.game-result');
+/*-------------------------------- Functions --------------------------------*/
+Init(); //initialize the game
+function Init() {
+    generateWord();
+    render();
+}
+
+//generate random word  from wordlist and a hint
+function generateWord() {
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    currentWord = wordList[randomIndex].word;
+    wordHint = wordList[randomIndex].hint;
+    guessedLetters = [];
+    wrongLetters = [];
+    remainingGuesses = maxWrongGuesses;
+    render();
+    //console.log(currentWord);
+    //current letter selected by player  didplay letter in placeholder otherwise chances are descresed
+
+    const wordPlaceholder = currentWord.split('').map(letter => {
+        return guessedLetters.includes(letter) ? letter : '_';
+    }).join('');
+    wordDispleyEl.textContent = wordPlaceholder;
+
+    //console.log(wordPlaceholder);
+    //word guessed by player then congrats message
+    if (wordPlaceholder === currentWord) {
+        alert('Congratulations! You guessed the word!');
 
     }
-];
-const maxWrongGuesses = 6;
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-/*-------------------------------- Variables --------------------------------*/
-let guessedWord, spaceship, wrongLetters, currentWord;
-let wrongGuesses = 0;
-let correctGuesses = 0;
-let guessesRemaining = maxWrongGuesses;
-let lettersInWord = 0;
-let lettersGuessedNotInWord = 0;
-let lettersGuessedInWord = 0;
-let lettersRemaining = 0;
 
+} 
+//render the game state 
+//i want to disable button if key is already pressed
+function  handlekeyClicked(event){
+    if (event.key === 'Enter') {
+        generateWord();
+    }
+    if (alphabet.includes(event.key)) {
+        handlePlayerSelection(event.key);
+    }
+} 
 
-
-/*------------------------ Cached Element References ------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+document.querySelector('main').adEventlistener(  'click',handlekeyClicked);
+playAgainBtn.addEventListener('click', generateWord);
+document.querySelector
